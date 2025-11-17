61b627d281880bed1eb157e3d5543fe37356d985
JoseAntonio1DAM <usr4034@salesianos-lacuesta.net>
2025-11-17 17:01:59 +0000

    hecho el exp 1

---
+++ src/net/salesianos/utils/ExperimentoUno.java
@@ -0,0 +1,28 @@
+package net.salesianos.utils;
+
+import net.salesianos.fruteria.Almacen;
+import net.salesianos.hilos.Consumidor;
+import net.salesianos.hilos.Productor;
+
+public class ExperimentoUno {
+    public static void main(String[] args) {
+        System.out.println("🔬 EXPERIMENTO 1: Un productor y un consumidor\n");
+        
+        Almacen almacen = new Almacen(5);
+        
+        Productor productor = new Productor("Único Productor", almacen, 5, 500);
+        Consumidor consumidor = new Consumidor("Único Consumidor", almacen, 5, 500);
+        
+        productor.start();
+        consumidor.start();
+        
+        try {
+            productor.join();
+            consumidor.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        
+        System.out.println("\n🏁 Experimento 1 completado");
+    }
+}
